1628d4e3fbb9d25cb90ebaa4253d9f0c1d104350
shrads <tshradheya@gmail.com>
2022-11-27 15:22:15 +0800

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::collections::HashSet;
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let result = y2020();
+
+    println!("{}", result);
+}
+
+fn y2020() -> i32 {
+    let file_name = "input/y2020q1a.txt";
+    println!("Reading file {}", file_name);
+
+    let f = File::open(file_name).unwrap();
+    let reader = BufReader::new(f);
+
+    let input: Vec<i32> = reader.lines()
+        .map(|l| l.unwrap().parse::<i32>().unwrap()).collect();
+
+    let mut hash_set: HashSet<i32> = HashSet::new();
+
+    let target = 2020;
+
+    for i in input {
+        let remaining_sum = target - i;
+        if hash_set.contains(&remaining_sum) {
+            return i * remaining_sum;
+        }
+        hash_set.insert(i);
+    }
+    0
 }
